b68d5b0f540073bea8167d88c65909efd20a6276
je6bmq <>
2017-08-14 16:10:11 +0900

    add test condition

--- src/main.rs
+++ src/main.rs
@@ -408,6 +408,7 @@ fn validation_test() {
     assert!(axes_validator("1:2".to_string()).is_ok());
     assert!(colors_validator("red,f8Ab05".to_string()).is_ok());
     assert!(width_validator("1.00".to_string()).is_ok());
+    assert!(width_validator("1".to_string()).is_ok());
 
     assert!(axes_validator("1:2,3".to_string()).is_err());
     assert!(colors_validator("lered,aaaagg".to_string()).is_err());
